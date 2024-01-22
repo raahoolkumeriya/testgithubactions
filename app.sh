01f371b8122b6733316afb1c10269c0ea3fb634e
raahoolkumeriya <kumeriya_rahul@rediffmail.com>
2024-01-22 19:34:41 +0530

    Startup code 4

--- app.sh
+++ app.sh
@@ -3,5 +3,6 @@
 echo "[$(date '+%Y-%d-%m %H-%M-%S')]: ( $(basename $0) ): Hello From Script!"
 echo "[$(date '+%Y-%d-%m %H-%M-%S')]: ( $(basename $0) ): Hello There!"
 echo "[$(date '+%Y-%d-%m %H-%M-%S')]: ( $(basename $0) ): Hello !"
+echo "[$(date '+%Y-%d-%m %H-%M-%S')]: ( $(basename $0) ): Hello !"
 
 exit $?
