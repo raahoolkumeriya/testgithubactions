8538455a42122269a83060900e224620daaf1ff9
raahoolkumeriya <kumeriya_rahul@rediffmail.com>
2024-01-22 19:46:00 +0530

    Startup code 5

--- app.sh
+++ app.sh
@@ -1,8 +1,11 @@
 #!/bin/bash 
 
 echo "[$(date '+%Y-%d-%m %H-%M-%S')]: ( $(basename $0) ): Hello From Script!"
+echo
 echo "[$(date '+%Y-%d-%m %H-%M-%S')]: ( $(basename $0) ): Hello There!"
+echo
 echo "[$(date '+%Y-%d-%m %H-%M-%S')]: ( $(basename $0) ): Hello !"
+echo
 echo "[$(date '+%Y-%d-%m %H-%M-%S')]: ( $(basename $0) ): Hello !"
 
 exit $?
